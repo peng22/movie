3816d2c8a060b84c8975ecb706c987a7857685ee
Mohamed Farid <mohamed_mido22107@yahoo.com>
2022-02-02 11:48:39 +0000

    uploading code

---
+++ movies/api/permissions.py
@@ -0,0 +1,24 @@
+from rest_framework import permissions
+
+
+class UserFieldPermission(permissions.BasePermission):
+    user_field = None
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Get the "owner" of the object from the user_field
+        return getattr(obj, self.user_field) == request.user
+
+
+# existing code omitted
+
+class IsCreatorPermission(UserFieldPermission):
+    user_field = "creator"
+
+
+class IsInviteePermission(UserFieldPermission):
+    user_field = "invitee"
+
+    
